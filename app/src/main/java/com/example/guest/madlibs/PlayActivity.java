aa01169ff331f6ea174215b4368562debbe2d5f5
charles carter and david remington <david.ian.remington@gmail.com>
2016-04-19 16:24:49 -0700

    add click listener to close soft keyboard

--- app/src/main/java/com/example/guest/madlibs/PlayActivity.java
+++ app/src/main/java/com/example/guest/madlibs/PlayActivity.java
@@ -1,10 +1,14 @@
 package com.example.guest.madlibs;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,11 +18,44 @@ public class PlayActivity extends AppCompatActivity {
     private EditText mEditTextAdj;
     private EditText mEditTextVerb;
 
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public void setupUI(View view) {
+
+        //Set up touch listener for non-text box views to hide keyboard.//
+        if(!(view instanceof EditText)) {
+
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(PlayActivity.this);
+                    return false;
+                }
+
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.//
+        if (view instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+                View innerView = ((ViewGroup) view).getChildAt(i);
+
+                setupUI(innerView);
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        setupUI(findViewById(R.id.parentPlayActivity));
 
         mEditTextNoun = (EditText) findViewById(R.id.editTextNoun);
         mEditTextAdj = (EditText) findViewById(R.id.editTextAdj);
--- app/src/main/java/com/example/guest/madlibs/ResultsActivity.java
+++ app/src/main/java/com/example/guest/madlibs/ResultsActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
     private TextView mResultsTextView;
+    private Button mPlayAgainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,17 @@ public class ResultsActivity extends AppCompatActivity {
         String verb = intent.getStringExtra("verb");
 
         mResultsTextView.setText("Herbert, Bartleby, and Johnifer were going to buy some " + noun + " at the " + adj + " store. They were hoping to " + verb + " their cheese. Oh wow, said Bartleby. What a quirky situation this is.");
+
+        mPlayAgainButton = (Button) findViewById(R.id.playAgainButton);
+        mPlayAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResultsActivity.this, MainActivity.class);
+                //Do this to clear all other loading activities in memory//
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
+
